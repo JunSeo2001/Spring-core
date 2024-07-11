76159c3dac33156ea390922fd63d9ec8f25f19c5
JunSeo2001 <162302440+JunSeo2001@users.noreply.github.com>
2024-07-11 17:58:18 +0900

    2024/0711

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -15,26 +15,28 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class AppConfig {
 
-    //설정용?
+    @Bean
+    public MemberService memberService() {
+        System.out.println("call AppConfig.memberService");
+        return new MemberServiceImpl(memberRepository());
+    }
+
     @Bean
     public MemoryMemberRepository memberRepository() {
+        System.out.println("call AppConfig.orderService");
         return new MemoryMemberRepository();
     }
+
+    @Bean
+    public OrderService orderService() {
+        System.out.println("call AppConfig.orderService");
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
     @Bean
     public DiscountPolicy discountPolicy() {
         return new RateDiscountPolicy();
     }
 
-
-    // 실제 불러오는 구조자
-    @Bean
-    public MemberService memberService() {
-        return new MemberServiceImpl(memberRepository());
-    }
-
-    @Bean
-    public OrderService orderService() {
-        return new OrderServiceImpl(memberRepository(), discountPolicy());
-    }
 
 }
